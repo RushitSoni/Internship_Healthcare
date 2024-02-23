5811dbf6f8e5ed99bde4545fc53b91e67e22c069
meet272002 <meet272002@gmail.com>
2024-02-23 22:09:11 +0530

    minor changes

--- ClientSide/Online_Survey/src/app/create/create.module.ts
+++ ClientSide/Online_Survey/src/app/create/create.module.ts
@@ -4,7 +4,7 @@ import { ClipboardModule } from 'ngx-clipboard';
 import { CreateRoutingModule } from './create-routing.module';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { GenerateSurveyComponent } from './generate-survey/generate-survey.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CompletionComponent } from './completion/completion.component';
 import { DisplayComponent } from './display/display.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -24,6 +24,7 @@ import { MatInputModule } from '@angular/material/input';
     CreateRoutingModule,
     ReactiveFormsModule,
     ClipboardModule,
+    FormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatInputModule
--- ClientSide/Online_Survey/src/app/create/generate-survey/generate-survey.component.ts
+++ ClientSide/Online_Survey/src/app/create/generate-survey/generate-survey.component.ts
@@ -10,16 +10,35 @@ import { Router } from '@angular/router';
 })
 export class GenerateSurveyComponent implements OnInit{
 
-  set = false;
+  selectedDate : string = '';
+  set = true;
+  mindate: String;
+selectedTime: any;
   constructor(private service: CreateService,private globalService: GlobalserviceService,private router : Router)
   {
-
+    this.mindate = this.formatDate(new Date());
   }
 
   ngOnInit(): void {
     
   }
 
+  formatDate(date : Date) : String {
+    const year = date.getFullYear();
+    // console.log(date.getFullYear());
+    const month = date.getMonth() + 1;
+    // console.log(date.getMonth());
+    const day = date.getDate();
+    // console.log(date.getDate());
+    // console.log(day.toString());
+    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+  }
+
+  SetTime()
+  {
+    this.set = !this.set;
+  }
+  
   CreateSurvey()
   {
     this.service.createSurvey('ccc5dff2-a4c6-4c9c-882a-130bab6a2d26').subscribe((data)=>{
@@ -29,8 +48,5 @@ export class GenerateSurveyComponent implements OnInit{
     this.router.navigate(['/create/generate','addquestion']);
   }
 
-  SetTime()
-  {
-    this.set = !this.set;
-  }
+  
 }
